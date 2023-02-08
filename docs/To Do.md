e7388d2c7f3d8824db458ff1df435ac5034b2a6c
Dev-Gabriel-Braga <gabriel.braga.matos@gmail.com>
2023-02-08 09:40:23 -0300

    translate: Coordenada dos Nodes em Notação Científica
    Na escrita do arquivo .dat, agora as coordenadas dos nodes são formatadas em notação científica.

--- docs/To Do.md
+++ docs/To Do.md
@@ -1,3 +1,5 @@
 # To Do - Coisas para Adicionar
 
+- Suporte a Materiais e Seções nas traduções do "translate".
+- Uma database para mensagens de ajuda para cada comando.
 - Um modo interativo.
--- lmcv_tools/models/node.py
+++ lmcv_tools/models/node.py
@@ -14,11 +14,14 @@ class Node(Entity):
 
       # Tratando Dados e Construindo a Seção dat
       n_nodes = len(self.raw_data)
+      span = len(str(n_nodes))
       coords = ''
 
       for coord in self.raw_data:
          info = list(map(float, coord))
          info[0] = int(info[0])
-         coords += '{0}   {1}   {2}   {3}\n'.format(*info)
+         offset = span - len(str(info[0]))
+         offset = ' ' * offset
+         coords += '{0}{4}   {1:+.8e}   {2:+.8e}   {3:+.8e}\n'.format(*info, offset)
       
       self.dat_entity = self.dat_template.format(n_nodes, n_nodes, coords)
