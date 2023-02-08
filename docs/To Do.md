32036179a5898b1c56cc5c2680c5ed2a99add7f0
Dev-Gabriel-Braga <gabriel.braga.matos@gmail.com>
2023-02-08 14:22:16 -0300

    translate: Suporte para Tradução de Elementos S3 e STRI65 (T3, T6)

--- docs/To Do.md
+++ docs/To Do.md
@@ -2,4 +2,5 @@
 
 - Suporte a Materiais e Seções nas traduções do "translate".
 - Uma database para mensagens de ajuda para cada comando.
+- Um módulo na interface para lidar apenas com a gestão de arquivos.
 - Um modo interativo.
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -25,10 +25,22 @@ class DefaultTest(unittest.TestCase):
       os.remove(dat_path)
 
 class Test2D(DefaultTest):
+   def test_triangle_S3_1x1(self):
+      self.default_test('Triangle_S3_1x1')
+
+   def test_triangle_S3_2x2(self):
+      self.default_test('Triangle_S3_2x2')
+
+   def test_triangle_STRI65_1x1(self):
+      self.default_test('Triangle_STRI65_1x1')
+
+   def test_triangle_STRI65_2x2(self):
+      self.default_test('Triangle_STRI65_2x2')
+
    def test_square_S4_1x1(self):
       self.default_test('Square_S4_1x1')
    
-   def test_square_S8_2x2(self):
+   def test_square_S4_2x2(self):
       self.default_test('Square_S4_2x2')
 
    def test_square_S8R_1x1(self):
@@ -37,6 +49,9 @@ class Test2D(DefaultTest):
    def test_square_S8R_2x2(self):
       self.default_test('Square_S8R_2x2')
    
+   def test_circle_S3_S4R_4x4(self):
+      self.default_test('Circle_S3_S4R_4x4')
+
    def test_complex_part_S8R_2x2(self):
       self.default_test('ComplexPart_S8R')
 
@@ -53,5 +68,5 @@ class Test3D(DefaultTest):
    def test_cube_C3D20_2x2x2(self):
       self.default_test('Cube_C3D20_2x2x2')
 
-   def test_complex_part_C2D20R(self):
+   def test_complex_part_C3D20R(self):
       self.default_test('ComplexPart_C3D20R')
