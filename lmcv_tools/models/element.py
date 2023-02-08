0ba3b8b50cd82e7665dee9f31b973b077cc60053
Dev-Gabriel-Braga <gabriel.braga.matos@gmail.com>
2023-02-08 10:41:57 -0300

    translate: Melhorias na Database de Relacionamento de elementos
    A database de relacionamento de elementos aramazenava informações sobre reorganização dos id de nodes de um jeito pouco eficiente e mais trabalhoso para adicionar suporte a novos elementos. Sua estrutura foi então revisava e algumas adaptações no model element.py foram realizadas pra permitir isso.

--- lmcv_tools/models/element.py
+++ lmcv_tools/models/element.py
@@ -16,8 +16,10 @@ class Element(Entity):
       # Relacionando Elementos inp com Elementos dat
       try:
          dat_element_type = self.element_relations['inp_to_dat'][inp_element_type]
-         dat_reformat = self.element_relations['dat_reformat'][dat_element_type]
-         self.inp_format = self.element_relations['inp_format'][inp_element_type]
+         dat_reference = self.element_relations['dat_reference'][dat_element_type]
+         int_id = '(\d+)'
+         node_id = ',\s*' + int_id
+         self.inp_format = int_id + dat_reference['n_nodes'] * node_id
       except KeyError:
          Messenger.error(f'Element {inp_element_type} in .inp file is not supported in this version.')
 
@@ -26,11 +28,22 @@ class Element(Entity):
 
       # Tratando Dados e Construindo a Seção dat
       n_elements = len(self.raw_data)
+      span = len(str(n_elements))
       description = ''
 
       for element in self.raw_data:
          info = list(map(int, element))
-         description += dat_reformat.format(*info) + '\n'
+
+         # Inserindo Id do Elemento, Seção e Ordem de Integração
+         element_id = info.pop(0)
+         offset = span - len(str(element_id))
+         offset = ' ' * offset
+         description += f'{element_id}{offset}   1  1'
+
+         # Inserindo nova ordem de elementos
+         for index in dat_reference['reorder']:
+            description += f'   {info[index - 1]}'
+         description += '\n'
 
       self.dat_entity = self.dat_template.format(
          n_elements,
