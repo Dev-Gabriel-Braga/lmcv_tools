ff0c167854df9067058a806c694d3df27d0e9a53
Dev-Gabriel-Braga <gabriel.braga.matos@gmail.com>
2023-10-08 00:06:24 -0300

    Atualização do Número da Versão.

--- lmcv_tools/interface/core.py
+++ lmcv_tools/interface/core.py
@@ -2,7 +2,7 @@ from . import messenger
 from ..models.custom_errors import CommandError
 
 # Constantes Globais
-version = '0.0.15'
+version = '0.0.16'
 in_interactive_mode = False
 message_welcome = '''
 LMCV Tools is a command line tool that provides a series of useful functionali-
